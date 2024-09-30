e9456ab78230623d2608b003318a89bc65bff999
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-30 15:10:35 +0300

    localization for exceptions

--- src/main/java/com/colak/springlocaletutorial/controller/TranslateController.java
+++ src/main/java/com/colak/springlocaletutorial/controller/TranslateController.java
@@ -1,6 +1,9 @@
 package com.colak.springlocaletutorial.controller;
 
 import com.colak.springlocaletutorial.util.MessageUtil;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,5 +16,18 @@ public class TranslateController {
     @GetMapping(path = "/translate/{key}")
     public String translate(@PathVariable("key") String key) {
         return MessageUtil.getMessage(key);
+    }
+
+    @GetMapping(path = "/error")
+    public void getNotFoundError() {
+        var message = MessageUtil.getMessage("error.entity.not.found");
+        throw new RuntimeException(message);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleRecordNotFoundException(Exception exception) {
+
+        return ProblemDetail
+                .forStatusAndDetail(HttpStatusCode.valueOf(404), exception.getLocalizedMessage());
     }
 }
--- src/test/java/com/colak/springlocaletutorial/controller/TranslateControllerTest.java
+++ src/test/java/com/colak/springlocaletutorial/controller/TranslateControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Objects;
@@ -57,13 +58,33 @@ class TranslateControllerTest {
     void translateWithPathVariable(String input, String expected) {
         String getUrl = "/api/translate/translate/greeting" + Objects.toString(input, "");
 
-
         ResponseEntity<String> response = testRestTemplate.getForEntity(
                 getUrl,
                 String.class);
         String body = response.getBody();
         assertEquals(expected, body);
 
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            {
+                    " ,Entity not found!",
+                    "?language=fr,Entité non trouvée!",
+                    "?language=tr,Kayıt bulunamadı!",
+                    "?language=en,Entity not found!",
+
+            }
+    )
+    void getNotFoundError(String input, String expected) {
+        String getUrl = "/api/translate/error" + Objects.toString(input, "");
+
+        ResponseEntity<ProblemDetail> response = testRestTemplate.getForEntity(
+                getUrl,
+                ProblemDetail.class);
+        ProblemDetail body = response.getBody();
+        String detail = body.getDetail();
+        assertEquals(expected, detail);
     }
 
 }
